222116aa05b423ebd413dfe41b4ed5cc552146dd
Erkin1 <ovganoverkinbek@mail.ru>
2019-01-14 21:08:03 +0600

    Hello

---
+++  Acmp.ru/src/Test0.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+public class Test0 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++  Acmp.ru/src/Test5.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+}
